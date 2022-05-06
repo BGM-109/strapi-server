67bbc6024b09c97924313159c830fa9715f32b00
BGM-109 <vividxxxxx@gmail.com>
2022-05-06 23:47:00 +0900

    add gymwear

---
+++ src/api/gymwear/controllers/gymwear.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  gymwear controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::gymwear.gymwear');
---
+++ src/api/gymwear/services/gymwear.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gymwear service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::gymwear.gymwear');
